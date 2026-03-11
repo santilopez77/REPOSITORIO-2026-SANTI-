dcc82b856743b75e06b679ffc40c3bdb897de538
santilopez77 <santiago.lopezg1@uqvirtual.edu.co>
2026-03-11 14:07:24 -0500

    j

---
+++ Agenda/src/main/java/uniquindio/edu/co/model/Contacto.java
@@ -0,0 +1,12 @@
+package uniquindio.edu.co.model;
+
+public class Contacto {
+    private String nombre;
+    private String alias;
+    private String direccion;
+    private String telefono;
+    private String email;
+
+
+    
+}
